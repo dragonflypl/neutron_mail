66f92bee082bf5631875b4724d55be4b122ccb24
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-07-01 23:52:17 +0200

    fix: mobile optim

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
 function showLanguages() {
 	let langs = document.getElementById('langs')
 	console.log(langs.classList)
-	langs.classList.toggle('show')
+	langs.classList.toggle('show-langs')
 }
 
 function showMenu() {
 	let nav = document.getElementById('nav')
 	let auth = document.getElementById('auth')
 	// console.log(menu)
-	nav.classList.toggle('show')
-	auth.classList.toggle('show')
+	nav.classList.toggle('show-nav')
+	auth.classList.toggle('show-auth')
 }
